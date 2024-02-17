9bb72b52d721888ffe25c961d3bba2351292016d
Chad DeRosier <chad.derosier@tutanota.com>
2024-02-16 22:41:12 -0800

    seems to be working

---
+++ src/encoder.rs
@@ -0,0 +1,152 @@
+use std::cmp::min;
+use std::marker::PhantomData;
+use std::sync::atomic::AtomicI32;
+use std::sync::atomic::Ordering;
+use std::sync::{Arc, Mutex};
+use std::thread;
+use std::time::{Duration, Instant};
+
+use esp_idf_hal::delay::Delay;
+use esp_idf_hal::gpio::AnyInputPin;
+pub use esp_idf_hal::gpio::InputPin;
+use esp_idf_hal::pcnt::*;
+pub use esp_idf_hal::peripheral::Peripheral;
+pub use esp_idf_svc::hal::ledc::LedcDriver;
+use esp_idf_sys::EspError;
+
+use device::{Action, Device};
+
+const LOW_LIMIT: i16 = -100;
+const HIGH_LIMIT: i16 = 100;
+
+pub struct Encoder<'d> {
+    unit: PcntDriver<'d>,
+    approx_value: Arc<AtomicI32>,
+}
+
+impl<'d> Encoder<'d> {
+    pub fn new<PCNT: Pcnt>(
+        pcnt: impl Peripheral<P = PCNT> + 'd,
+        pin_a: impl Peripheral<P = impl InputPin> + 'd,
+        pin_b: impl Peripheral<P = impl InputPin> + 'd,
+    ) -> Result<Self, EspError> {
+        let mut unit = PcntDriver::new(
+            pcnt,
+            Some(pin_a),
+            Some(pin_b),
+            Option::<AnyInputPin>::None,
+            Option::<AnyInputPin>::None,
+        )?;
+        unit.channel_config(
+            PcntChannel::Channel0,
+            PinIndex::Pin0,
+            PinIndex::Pin1,
+            &PcntChannelConfig {
+                lctrl_mode: PcntControlMode::Reverse,
+                hctrl_mode: PcntControlMode::Keep,
+                pos_mode: PcntCountMode::Decrement,
+                neg_mode: PcntCountMode::Increment,
+                counter_h_lim: HIGH_LIMIT,
+                counter_l_lim: LOW_LIMIT,
+            },
+        )?;
+        unit.channel_config(
+            PcntChannel::Channel1,
+            PinIndex::Pin1,
+            PinIndex::Pin0,
+            &PcntChannelConfig {
+                lctrl_mode: PcntControlMode::Reverse,
+                hctrl_mode: PcntControlMode::Keep,
+                pos_mode: PcntCountMode::Increment,
+                neg_mode: PcntCountMode::Decrement,
+                counter_h_lim: HIGH_LIMIT,
+                counter_l_lim: LOW_LIMIT,
+            },
+        )?;
+
+        unit.set_filter_value(min(10 * 80, 1023))?;
+        //       unit.set_filter_value(min(1023/2, 1023))?;
+        unit.filter_enable()?;
+
+        let approx_value = Arc::new(AtomicI32::new(0));
+        // unsafe interrupt code to catch the upper and lower limits from the encoder
+        // and track the overflow in `value: Arc<AtomicI32>` - I plan to use this for
+        // a wheeled robot's odomerty
+        unsafe {
+            let approx_value = approx_value.clone();
+            unit.subscribe(move |status| {
+                let status = PcntEventType::from_repr_truncated(status);
+                if status.contains(PcntEvent::HighLimit) {
+                    approx_value.fetch_add(HIGH_LIMIT as i32, Ordering::SeqCst);
+                }
+                if status.contains(PcntEvent::LowLimit) {
+                    approx_value.fetch_add(LOW_LIMIT as i32, Ordering::SeqCst);
+                }
+            })?;
+        }
+        unit.event_enable(PcntEvent::HighLimit)?;
+        unit.event_enable(PcntEvent::LowLimit)?;
+        unit.counter_pause()?;
+        unit.counter_clear()?;
+        unit.counter_resume()?;
+
+        Ok(Self { unit, approx_value })
+    }
+
+    pub fn get_value(&self) -> Result<i32, EspError> {
+        let value =
+            self.approx_value.load(Ordering::Relaxed) + self.unit.get_counter_value()? as i32;
+        Ok(value)
+    }
+}
+
+pub fn update_slider_type_devices(
+    mut encoders: Vec<Encoder>,
+    devices: Arc<Mutex<Vec<Device>>>,
+    delay_ms: u32,
+) {
+    let mut last_encoder_values = Vec::from([0i32, 0i32]);
+    let mut last_encoder_times = Vec::from([Instant::now(), Instant::now()]);
+    let delay = Delay::new(delay_ms);
+    loop {
+        {
+            let mut devices_guard = devices.lock().unwrap();
+            for (((device, encoder), last_encoder_time), last_encoder_value) in devices_guard
+                .iter_mut()
+                .zip(encoders.iter_mut())
+                .zip(last_encoder_times.iter_mut())
+                .zip(last_encoder_values.iter_mut())
+            {
+                update_device(device, encoder, last_encoder_time, last_encoder_value);
+            }
+        }
+        //Delay::delay_ms(100u32);
+        Delay::delay_ms(&delay, delay_ms);
+    }
+}
+
+fn update_device(
+    device: &mut Device,
+    encoder: &mut Encoder,
+    last_encoder_time: &mut Instant,
+    last_encoder_value: &mut i32,
+) {
+    let encoder_value = encoder.get_value().unwrap();
+    if encoder_value != *last_encoder_value {
+        let current_time = Instant::now();
+        let time_since_last_check = current_time.duration_since(*last_encoder_time);
+        if time_since_last_check > Duration::from_millis(100) {
+            {
+                if encoder_value > *last_encoder_value {
+                    let _ = device.take_action(Action::Up { amount: None });
+                    device.updated = true;
+                } else {
+                    let _ = device.take_action(Action::Down { amount: None });
+                    device.updated = true;
+                }
+            }
+            *last_encoder_time = Instant::now();
+        }
+        *last_encoder_value = encoder_value;
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,294 @@
+use std::{
+    collections::HashMap,
+    default::Default,
+    sync::{Arc, Mutex},
+    thread,
+    thread::sleep,
+    time::{Duration, Instant},
+};
+
+use heapless;
+use uuid::Uuid;
+
+use embedded_svc::{
+    http::server::{Request},
+    http::Method,
+    io::{Error, Write},
+};
+pub use esp_idf_hal::ledc::{config::TimerConfig, LedcDriver, LedcTimerDriver};
+pub use esp_idf_hal::units::FromValueType;
+use esp_idf_hal::{delay::Delay, gpio::*, peripherals::Peripherals, units::Hertz, modem::Modem};
+use esp_idf_svc::{
+    eventloop::EspSystemEventLoop,
+    io::EspIOError,
+    http::server::{EspHttpConnection, EspHttpServer},
+    nvs::EspDefaultNvsPartition,
+    wifi::{ClientConfiguration, Configuration, EspWifi},
+};
+use esp_idf_svc::http::server::Configuration as SVC_Configuration;
+//pub use esp_idf_hal::ledc::{config::LedcDriver, LedcTimerDriver, TimerConfig};
+
+pub use device;
+use device::{Action, Device, DeviceType};
+
+pub mod encoder;
+//pub mod wrappers;
+//pub use encoder::{update_slider_type_device_from_encoder, Encoder, EncoderPeripheralData};
+
+pub struct Node {
+    //pub devices: Arc<Mutex<Vec<Device>>>,
+    pub ssid: String,
+    pub password: String,
+}
+
+impl Default for Node {
+    fn default() -> Self {
+        Self {
+            ssid: String::default(),
+            password: String::default(),
+        }
+    }
+}
+
+impl Node {
+    pub fn setup() -> Peripherals {
+        esp_idf_svc::sys::link_patches();
+
+        esp_idf_svc::log::EspLogger::initialize_default();
+
+        log::info!("hello log");
+        
+        Peripherals::take().unwrap()
+    }
+
+    //#[cfg(all(not(feature = "riscv-ulp-hal"), any(esp32, esp32s2, esp32s3)))]
+    pub fn run(&mut self, devices: Arc<Mutex<Vec<Device>>>, modem: Modem) {
+        let sys_loop = EspSystemEventLoop::take().unwrap();
+        let nvs =  EspDefaultNvsPartition::take().unwrap();
+        let mut wifi_driver =
+            EspWifi::new(modem, sys_loop, Some(nvs)).unwrap();
+        wifi_driver
+            .set_configuration(&Configuration::Client(ClientConfiguration {
+                ssid: heapless::String::try_from(self.ssid.as_str()).unwrap(),
+                password: heapless::String::try_from(self.password.as_str()).unwrap(),
+                ..Default::default()
+            }))
+            .unwrap();
+        wifi_driver.start().unwrap();
+        wifi_driver.connect().unwrap();
+        while !wifi_driver.is_connected().unwrap() {
+            let config = wifi_driver.get_configuration().unwrap();
+            println!("Waiting for station {:?}", config);
+        }
+        println!("Should be connected now");
+
+        let mut server = EspHttpServer::new(&SVC_Configuration::default()).unwrap();
+        /*
+         server
+        .fn_handler("/", Method::Get, |request| {
+            let mut response = request.into_ok_response()?;
+            response.write_all("payload!!!!!".as_bytes())?;
+            Ok(())
+        })
+        .unwrap();
+        */
+        let devices_clone = Arc::clone(&devices);
+        server
+            .fn_handler("/status", Method::Get, move |request| {
+                if &request.uri().len() < &8_usize {
+                    let _ = exit_early(request, "Bad Status command given", 422);
+                    //return Ok(());
+                    return Ok::<(), EspIOError>(());
+                }
+                let query = &request.uri()[8..].to_string().to_lowercase();
+                let query: HashMap<_, _> = querystring::querify(query).into_iter().collect();
+                if query.get("device").is_some() {
+                    let d = query.get("device").unwrap();
+                    let d = d.replace("%20", " ");
+                    for device in devices_clone.lock().unwrap().iter() {
+                        if device.name == d {
+                            let mut response = request.into_ok_response()?;
+                            let _ = response.write_all(&device.to_json().into_bytes()[..]);
+                            //return Ok(());
+                            return Ok::<(), EspIOError>(());
+                        }
+                    }
+                    let _ = exit_early(request, "Device name not found", 422);
+                    return Ok(());
+                } else if query.get("uuid").is_some() {
+                    let u = query.get("uuid").unwrap();
+                    for device in devices_clone.lock().unwrap().iter() {
+                        if &device.uuid.to_string().as_str() == u {
+                            let mut response = request.into_ok_response()?;
+                            let _ = response.write_all(&device.to_json().into_bytes()[..]);
+                            //return Ok(());
+                            return Ok::<(), EspIOError>(());
+                        }
+                    }
+                    let _ = exit_early(request, "Device name not found", 422);
+                    //return Ok(());
+                    return Ok::<(), EspIOError>(());
+                } else {
+                    let _ = exit_early(request, "No Device name given", 422);
+                    //return Ok(());
+                    return Ok::<(), EspIOError>(());
+                }
+            })
+            .unwrap();
+        let devices_clone = Arc::clone(&devices);
+        server
+            .fn_handler("/devices", Method::Get, move |request| {
+                let mut devices = HashMap::new();
+                {
+                    for device in devices_clone.lock().unwrap().iter() {
+                        devices.insert(device.name.clone(), device.clone());
+                    }
+                }
+                let payload = serde_json::json!(devices);
+                let mut response = request.into_ok_response()?;
+                response.write_all(payload.to_string().as_bytes())?;
+                //Ok(())
+                return Ok::<(), EspIOError>(());
+            })
+            .unwrap();
+        let devices_clone = Arc::clone(&devices);
+        server
+            .fn_handler("/command", Method::Get, move |request| {
+                if &request.uri().len() < &9_usize {
+                    let _ = exit_early(request, "Bad Command given", 422);
+                    //return Ok(());
+                    return Ok::<(), EspIOError>(());
+                }
+                let query = &request.uri()[9..].to_string();
+                let query: HashMap<_, _> = querystring::querify(query).into_iter().collect();
+                let target: Option<usize> = match query.get("target") {
+                    Some(t_text) => match t_text.parse::<usize>() {
+                        Ok(t_num) => {
+                            if t_num > 7 {
+                                let _ = exit_early(request, "Target must be >= 0 & < 8", 422);
+                                //return Ok(());
+                                return Ok::<(), EspIOError>(());
+                            }
+                            Some(t_num)
+                        }
+                        Err(_) => {
+                            if t_text != &"" {
+                                let _ = exit_early(request, "Bad Target given", 422);
+                                //return Ok(());
+                                return Ok::<(), EspIOError>(());
+                            }
+                            None
+                        }
+                    },
+                    None => None,
+                };
+                let action = match query.get("action") {
+                    Some(a) => match Action::from_str(&a.to_lowercase(), target) {
+                        Ok(a) => a,
+                        Err(_) => {
+                            let _ = exit_early(request, "Bad Action name given", 422);
+                            //return Ok(());
+                            return Ok::<(), EspIOError>(());
+                        }
+                    },
+                    None => {
+                        let _ = exit_early(request, "No Action given", 422);
+                        //return Ok(());
+                        return Ok::<(), EspIOError>(());
+                    }
+                };
+                match query.get("uuid") {
+                    Some(u) => match Uuid::parse_str(u) {
+                        Ok(uuid) => {
+                            for device in devices_clone.lock().unwrap().iter_mut() {
+                                if uuid == device.uuid {
+                                    let _ = device.take_action(action);
+                                    let mut response = request.into_ok_response()?;
+                                    let _ = response.write_all(&device.to_json().into_bytes());
+                                    //return Ok(());
+                                    return Ok::<(), EspIOError>(());
+                                }
+                            }
+                            let _ = exit_early(request, "Uuid not found among devices", 422);
+                            //`return Ok(());
+                            return Ok::<(), EspIOError>(());
+                        }
+                        Err(_) => {
+                            let _ = exit_early(request, "Bad Uuid given", 422);
+                            //return Ok(());
+                            return Ok::<(), EspIOError>(());
+                        }
+                    },
+                    None => {
+                        let _ = exit_early(request, "Uuid field not given", 422);
+                        //return Ok(());
+                        return Ok::<(), EspIOError>(());
+                    }
+                };
+            })
+            .unwrap();
+
+        loop {
+            println!(
+                "IP info: {:?}",
+                wifi_driver.sta_netif().get_ip_info().unwrap()
+            );
+            sleep(Duration::new(100, 0));
+        }
+    }
+}
+
+pub fn get_frequencies(devices: &Arc<Mutex<Vec<Device>>>) -> Vec<Hertz> {
+    devices
+        .lock()
+        .unwrap()
+        .iter()
+        .map(|d| d.freq_Hz.Hz())
+        .collect()
+}
+
+pub fn get_max_duty_cycles(drivers: &Vec<LedcDriver>) -> Vec<u32> {
+    let mut max_duty_cycles = Vec::with_capacity(drivers.len());
+    for driver in drivers {
+        max_duty_cycles.push(driver.get_max_duty());
+    }
+    max_duty_cycles
+}
+
+pub fn update_device_duty_cycles(
+    devices: Arc<Mutex<Vec<Device>>>,
+    mut drivers: Vec<LedcDriver>,
+    max_duty_cycles: Vec<u32>,
+    delay_ms: u32,
+) {
+    let delay = Delay::new(100);
+    loop {
+        {
+            for ((device, driver), max_duty) in devices
+                .lock()
+                .unwrap()
+                .iter_mut()
+                .zip(drivers.iter_mut())
+                .zip(max_duty_cycles.iter())
+            {
+                if device.updated {
+                    println!("Updating: {:?}", device);
+                    let duty_cycle = device.get_duty_cycle() * max_duty / 100;
+                    let _ = driver.set_duty(duty_cycle);
+                    device.updated = false;
+                }
+            }
+        }
+        delay.delay_ms(delay_ms);
+    }
+}
+
+fn exit_early<'a>(
+    request: Request<&mut EspHttpConnection<'a>>,
+    message: &str,
+    code: u16,
+) -> Result<(), EspIOError> {
+    let mut response = request.into_status_response(code)?;
+    let _ = response.write_all(message.as_bytes());
+    Ok(())
+}
